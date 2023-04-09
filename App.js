fb0aab30614ccec112f59b0e75a03d6e93db0d41
Demet Çalışkan <demet@aposto.com>
2023-04-10 01:10:51 +0300

    add navigation

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
+import SignupScreen from './src/screens/SignupScreen'
+import SigninScreen from './src/screens/SigninScreen'
+import TrackCreateScreen from './src/screens/TrackCreateScreen'
+import AccountScreen from './src/screens/AccountScreen'
+import TrackListScreen from './src/screens/TrackListScreen'
+import TrackDetailScreen from './src/screens/TrackDetailScreen'
+
+const Stack = createStackNavigator()
+const Tab = createMaterialBottomTabNavigator()
+
+const trackListNavigation = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name='TrackList' component={TrackListScreen} />
+      <Stack.Screen name='TrackDetail' component={TrackDetailScreen} />
+    </Stack.Navigator>
+  )
+}
+
+const bottomTabNavigation = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name='Account' component={AccountScreen} />
+      <Tab.Screen name='TrackCreate' component={TrackCreateScreen} />
+      <Tab.Screen name='Track' component={trackListNavigation} />
+    </Tab.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Signin' component={SigninScreen} />
+        <Stack.Screen name='Signup' component={SignupScreen} />
+        <Stack.Screen name='Home' component={bottomTabNavigation} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/SigninScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const SigninScreen = () => {
+  return <Text>Signin Screen</Text>
+}
+
+const styles = StyleSheet.create({})
+
+export default SigninScreen
